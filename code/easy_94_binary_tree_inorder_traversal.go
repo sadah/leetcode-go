a295d560fcbf7a1765051214f398c5676fa5a296
sadah <sadaaki.hirai@gmail.com>
2022-10-28 12:22:44 +0900

    Add

---
+++ code/easy_94_binary_tree_inorder_traversal.go
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=94 lang=golang
+ *
+ * [94] Binary Tree Inorder Traversal
+ */
+
+package code
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func inorderTraversal(root *TreeNode) []int {
+	var nums = []int{}
+	if root == nil {
+		return nums
+	}
+
+	nums = append(nums, inorderTraversal(root.Left)...)
+	nums = append(nums, root.Val)
+	nums = append(nums, inorderTraversal(root.Right)...)
+	return nums
+}
+
+// @lc code=end
