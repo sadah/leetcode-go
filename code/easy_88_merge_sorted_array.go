6490b37a575978060570592740cbe9f961066ae7
sadah <sadaaki.hirai@gmail.com>
2022-11-02 18:20:31 +0900

    Fix

--- code/easy_88_merge_sorted_array.go
+++ code/easy_88_merge_sorted_array.go
@@ -15,7 +15,7 @@ import (
 func merge(nums1 []int, m int, nums2 []int, n int) {
 	nums1 = nums1[0:m]
 	nums1 = append(nums1, nums2...)
-	sort.Sort(sort.IntSlice(nums1))
+	sort.Ints(nums1)
 }
 
 // @lc code=end
