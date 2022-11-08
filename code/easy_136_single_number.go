b1c5aa3f94508e4801b04d0937d6ed1453811cf7
sadah <sadaaki.hirai@gmail.com>
2022-11-08 12:47:41 +0000

    Add

---
+++ code/easy_136_single_number.go
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=136 lang=golang
+ *
+ * [136] Single Number
+ */
+
+package code
+
+// @lc code=start
+//
+//lint:ignore U1000 //
+func singleNumber(nums []int) int {
+	m := map[int]bool{}
+	for _, n := range nums {
+		if !m[n] {
+			m[n] = true
+		} else {
+			delete(m, n)
+		}
+	}
+	ret := 0
+	for k, _ := range m {
+		ret = k
+	}
+	return ret
+}
+
+// @lc code=end
