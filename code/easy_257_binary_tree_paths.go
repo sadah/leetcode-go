e438253f9a7a52a2cfea8dbf0d51d33d29f71054
sadah <sadaaki.hirai@gmail.com>
2022-11-25 15:49:36 +0900

    Add

---
+++ code/easy_257_binary_tree_paths.go
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode id=257 lang=golang
+ *
+ * [257] Binary Tree Paths
+ */
+
+package code
+
+import (
+	"strconv"
+	"strings"
+)
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+func binaryTreePaths(root *TreeNode) []string {
+	if root == nil {
+		return nil
+	}
+	return walkBTree(root, []string{strconv.Itoa(root.Val)}, []string{})
+}
+
+func walkBTree(root *TreeNode, strs []string, ret []string) []string {
+	if root.Left == nil && root.Right == nil {
+		s := strings.Join(strs, "->")
+		return append(ret, s)
+	}
+	if root.Left != nil {
+		s := append(strs, strconv.Itoa(root.Left.Val))
+		ret = walkBTree(root.Left, s, ret)
+	}
+	if root.Right != nil {
+		s := append(strs, strconv.Itoa(root.Right.Val))
+		ret = walkBTree(root.Right, s, ret)
+	}
+	return ret
+}
+
+// @lc code=end
