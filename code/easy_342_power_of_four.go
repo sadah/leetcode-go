127e5aed77a79e9efb8fdfee48a6269c7cd05c0f
sadah <sadaaki.hirai@gmail.com>
2022-12-05 11:46:24 +0000

    Add

---
+++ code/easy_342_power_of_four.go
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=342 lang=golang
+ *
+ * [342] Power of Four
+ */
+
+// @lc code=start
+func isPowerOfFour(n int) bool {
+	if n <= 0 {
+		return false
+	}
+	if n == 1 {
+		return true
+	}
+	if n%4 != 0 {
+		return false
+	}
+	for n > 1 {
+		if n%4 != 0 {
+			return false
+		}
+		n = n / 4
+	}
+	return true
+}
+// @lc code=end
